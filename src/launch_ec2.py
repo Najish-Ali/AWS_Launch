a5e37ebbb69e85ae8242c9f8b661e270b616a73f
Najish Ali <sayyednajishali@gmail.com>
2025-04-08 13:59:34 +0530

    Update launch_ec2.py

--- src/launch_ec2.py
+++ src/launch_ec2.py
@@ -1,27 +1,30 @@
-# launch_ec2_instances.py
 import boto3
+import os
 
-# Take user inputs
-region = input("Enter the AWS region (e.g., ap-south-1): ")
-num_instances = int(input("Enter the number of instances to launch: "))
-ami_id = input("Enter the custom AMI ID: ")
-#instance_type = input("Enter the instance type (e.g., t2.micro): ")
-#key_name = input("Enter the key pair name (for SSH access): ")
+def launch_ec2_instances(region, num_instances, ami_id, instance_type, key_name, security_group_ids):
+    ec2 = boto3.client('ec2', region_name=region)
+    response = ec2.run_instances(
+        ImageId=ami_id,
+        MinCount=num_instances,
+        MaxCount=num_instances,
+        InstanceType=instance_type,
+        KeyName=key_name,
+        SecurityGroupIds=security_group_ids
+    )
+    instance_ids = [instance['InstanceId'] for instance in response['Instances']]
+    for idx, instance_id in enumerate(instance_ids, start=1):
+        print(f"  Instance {idx}: {instance_id}")
 
-# Initialize EC2 client
-ec2 = boto3.client('ec2', region_name=region)
+if __name__ == "__main__":
+    region = os.getenv('AWS_REGION', 'us-west-2')
+    num_instances = int(os.getenv('NUM_INSTANCES', 1))
+    ami_id = os.getenv('AMI_ID', '')
+    instance_type = os.getenv('INSTANCE_TYPE', 't2.micro')
+    key_name = os.getenv('KEY_NAME', 'my-key-pair')
+    security_group_ids = os.getenv('SECURITY_GROUP_IDS', 'sg-12345678').split(',')
 
-# Launch instances using a custom AMI
-response = ec2.run_instances(
-    ImageId=ami_id,  # Specify the AMI ID here
-    MinCount=num_instances,
-    MaxCount=num_instances,
-    InstanceType="t2.micro",  # Instance type
-    KeyName="test",  # SSH key pair for access
-    SecurityGroupIds=["sg-0cd5a1a0c2a934f7d"]
-)
+    if not ami_id:
+        print("AMI ID must be provided!")
+        exit(1)
 
-# Store and print launched instance IDs
-instance_ids = [instance['InstanceId'] for instance in response['Instances']]
-for idx, instance_id in enumerate(instance_ids, start=1):
-    print(f"Launched EC2 instance {idx}: {instance_id}")
+    launch_ec2_instances(region, num_instances, ami_id, instance_type, key_name, security_group_ids)
