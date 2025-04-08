21ad819603ceef27af6bba974d887c79db853f61
Najish Ali <sayyednajishali@gmail.com>
2025-04-08 12:58:55 +0530

    Delete src/list_buckets.py

--- src/list_buckets.py
+++
@@ -1,9 +0,0 @@
-def list_s3_buckets(s3_client):
-    """List all buckets in the S3 account."""
-    try:
-        response = s3_client.list_buckets()
-        buckets = [bucket['Name'] for bucket in response['Buckets']]
-        return buckets
-    except Exception as e:
-        print(f"Error listing buckets: {e}")
-        return []
